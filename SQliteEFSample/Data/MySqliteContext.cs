6df66c1677ba6eb4aa8e79a0f049b2b900c57c62
ANG3L-GAMBOA <angelxdangerxdgamara@gmail.com>
2025-11-21 13:07:59 -0500

    Proyect Cars

---
+++ SQliteEFSample/Data/MySqliteContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SQliteEFSample.Models;
+
+namespace SQliteEFSample.Data
+{
+    public class MySqliteContext : DbContext
+    {
+        public MySqliteContext (DbContextOptions<MySqliteContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SQliteEFSample.Models.Car> Car { get; set; } = default!;
+    }
+}
---
+++ SQliteEFSample/Migrations/20251121174512_Init.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SQliteEFSample.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Car",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Model = table.Column<string>(type: "TEXT", nullable: false),
+                    Make = table.Column<string>(type: "TEXT", nullable: false),
+                    Color = table.Column<string>(type: "TEXT", nullable: false),
+                    Year = table.Column<int>(type: "INTEGER", nullable: false),
+                    Doors = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Car", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Car");
+        }
+    }
+}
